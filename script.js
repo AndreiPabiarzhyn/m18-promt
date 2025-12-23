b2c7d3ed277ce6643cbe34a96127589eafbd9c86
Andrei <111114285+AndreiPabiarzhyn@users.noreply.github.com>
2025-12-23 17:22:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-document.getElementById("copyBtn").addEventListener("click", copyText);
+const copyBtn = document.getElementById("copyBtn");
+const copyLabel = document.getElementById("copyLabel");
+
+copyBtn.addEventListener("click", copyText);
 
 function copyText() {
-    const text = 
+    const text =
 `Придумай сказку на тему ${t1.value}.
 Главный герой — ${t2.value}, он ${t3.value}.
 Его друг — ${t4.value}.
 Однажды с ними произошло ${t5.value}.
 В конце истории всё закончилось так: ${t6.value}.`;
 
-    navigator.clipboard.writeText(text)
-        .then(() => alert("Текст скопирован!"));
+    navigator.clipboard.writeText(text).then(() => {
+        // анимация кнопки
+        copyBtn.classList.add("success");
+        setTimeout(() => copyBtn.classList.remove("success"), 600);
+
+        // показать надпись
+        copyLabel.classList.add("show");
+        setTimeout(() => copyLabel.classList.remove("show"), 2000);
+    });
 }
+
